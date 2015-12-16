3436cea4f2c5df68b3f92ebc9761083da69c8d02
THEBAUD Fabien <thebaudf@sib.fr>
2015-12-16 10:43:11 +0100

    ajout du grunt clean et corrections mineures

--- gruntfile.js
+++ gruntfile.js
@@ -5,10 +5,14 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     // Project configuration.  
     grunt.initConfig({  
         pkg: grunt.file.readJSON('package.json'),  
+        clean: {
+            release: ["dist"]
+        },
         copy: {
             externalresources : {
                 files: [{
@@ -20,21 +24,39 @@ module.exports = function (grunt) {
                 },{
                     src: 'node_modules/bootstrap/dist/css/bootstrap.min.css',
                     dest: 'src/external-resources/css/bootstrap.min.css'
-                }],           
-                dist: {
+                }]
+            },
+            release: {
                     src: 'src/index.html',
                     dest: 'dist/index.html'
-                }
-            },
+            }
         },
         cssmin: {  
-
+            sitecss: {  
+                options: {  
+                    banner: '/* My minified css file */'  
+                },  
+                files: {  
+                    'dist/css/site.min.css': [  
+                        'node_modules/bootstrap/dist/css/bootstrap.min.css'
+                    ]
+                }  
+            }  
         },  
-        uglify: {  
-
+        uglify: { 
+            options: {  
+                compress: true  
+            },  
+            applib: {  
+                src: [  
+                    'node_modules/jquery/dist/jquery.min.js',
+                    'node_modules/bootstrap/dist/js/bootstrap.min.js'
+                ],  
+                dest: 'dist/js/everything.js'  
+            } 
         }  
     });  
     
     // Default task.  
-    grunt.registerTask('default', ['copy:dist']);  
+    grunt.registerTask('default', ['clean:release', 'copy:release', 'uglify', 'cssmin']);  
 };
