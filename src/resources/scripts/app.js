befc5cd3ed83539d353ce653aff66c706e8bd9d0
Fabien Thebaud <fthebaud@gmail.com>
2015-12-16 17:21:11 +0100

    ajout input text

--- src/resources/scripts/app.js
+++ src/resources/scripts/app.js
@@ -1,5 +1,18 @@
 var secretsanta = {
-    alert : function(text){
-        $( ".container" ).append( "<div class='alert alert-success fade in'> <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>" + text + "</div>");
+    
+    init : function() {
+        $('input[name=participant]').bind("keypress", function(event) {
+            if(event.which == 13) {
+                event.preventDefault();
+                secretsanta.addParticipant();
+            }
+        });
+    },
+    
+    addParticipant : function(text){
+        var participant = $('input[name=participant]').val();
+        if (participant){
+            $( ".container" ).append( "<div class='alert alert-success fade in'> <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>" + participant + "</div>");    
+        }
     }
 }
