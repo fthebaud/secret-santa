b2d880ac7dc2a950d05fc8a2b4ba5dbb77de9fcb
Fabien Thebaud <fthebaud@gmail.com>
2015-12-21 16:44:30 -0800

    passage au mode strict

--- src/resources/scripts/app.js
+++ src/resources/scripts/app.js
@@ -1,27 +1,33 @@
-var secretsanta = {
+(function(){
+  /*global $:false */
+  "use strict";
 
-    //TODO make private, use modules... (ES6)
-    nbParticipants : 0,
-    
-    init : function() {
-        $('input[name=participant]').bind("keypress", function(event) {
-            if(event.which == 13) {
-                event.preventDefault();
-                secretsanta.addParticipant();
-            }
-        });
-    },
-    
-    addParticipant : function(text){
-        var participant = $('input[name=participant]').val();
-        $('input[name=participant]').val('');
-        if (participant){
-            secretsanta.nbParticipants++;
-            $( "#participants" ).prepend( "<tr><th>" + secretsanta.nbParticipants + "</th><td>" + participant + "</td><td></td><td><button type='button' onclick='secretsanta.deleteLine(" + secretsanta.nbParticipants + ")'>DELETE</button></td></tr>" );    
-        }
-    },
-    
-    deleteLine : function(lineNumber){
-        alert("delete line " + lineNumber);
-    }
-}
+  var secretsanta = {
+
+      //TODO make private, use modules... (ES6)
+      nbParticipants : 0,
+
+      init : function() {
+          $('input[name=participant]').bind("keypress", function(event) {
+              if(event.which == 13) {
+                  event.preventDefault();
+                  secretsanta.addParticipant();
+              }
+          });
+      },
+
+      addParticipant : function(text){
+          var participant = $('input[name=participant]').val();
+          $('input[name=participant]').val('');
+          if (participant){
+              secretsanta.nbParticipants++;
+              $( "#participants" ).prepend( "<tr><th>" + secretsanta.nbParticipants + "</th><td>" + participant + "</td><td></td><td><button type='button' onclick='secretsanta.deleteLine(" + secretsanta.nbParticipants + ")'>DELETE</button></td></tr>" );
+          }
+      },
+
+      deleteLine : function(lineNumber){
+          alert("delete line " + lineNumber);
+      }
+  };
+
+}());
